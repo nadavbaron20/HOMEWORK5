0c4cc1e7ff2ec0bd95b4528d5509addeb4f0ecd8
nadavbaron20 <114684480+nadavbaron20@users.noreply.github.com>
2022-11-14 17:41:07 +0200

    HOMEWORK5

---
+++ SCRIPT.js
@@ -0,0 +1,8 @@
+let a = Number(prompt('Please insert a number first number'));
+let b = Number(prompt('Please insert a number second number'));
+if ((a % b ) != 0) {
+  alert('There is a remainder');
+} else {
+  alert("There is no a remainder");
+}
+
